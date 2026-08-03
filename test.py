939ea1a3e4253faea8d7dd952d391c9126f4de8a
jayaganiger2024-coder <jaya.ganiger2024@gmail.com>
2026-08-03 10:55:50 +0530

    test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import streamlit as st
 st.title("My first stream lit app")
 name=st.text_input("Enter your name")
-if st.button("Submit"):
-  st.write(f"Hello, {name}")
 age=st.number_input("Enter your age")
 if st.button("Submit"):
   if (age>=18):
