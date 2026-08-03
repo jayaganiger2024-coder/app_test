57d1866ceae5db1acb06d6bc950b6bf6e9f818a7
jayaganiger2024-coder <jaya.ganiger2024@gmail.com>
2026-08-03 10:52:31 +0530

    test.py

--- test.py
+++ test.py
@@ -3,3 +3,7 @@ st.title("My first stream lit app")
 name=st.text_input("Enter your name")
 if st.button("Submit"):
   st.write(f"Hello, {name}")
+age=st.text_input("Enter your age")
+if age>=18:
+  st.write("You are eligible to vote")
+  
