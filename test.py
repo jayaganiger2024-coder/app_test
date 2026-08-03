5aef7caa8af2be93c4ec21f28f2b9ffbe264bec1
jayaganiger2024-coder <jaya.ganiger2024@gmail.com>
2026-08-03 10:53:24 +0530

    test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ name=st.text_input("Enter your name")
 if st.button("Submit"):
   st.write(f"Hello, {name}")
 age=st.text_input("Enter your age")
-if age>=18:
+if (age>=18):
   st.write("You are eligible to vote")
   
